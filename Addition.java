8b0027073397ce1382139d27a26d50ebc6634bd8
blessy <blessy.ble79@gmail.com>
2022-07-14 18:33:26 +0530

    Added a comment to Addition.java

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ class Addition
 public static void (string args [])
 {
 int num1,num2,res;
-num1=20;
-num2=45;
+num1=2005;
+num2=4505;
 res=num1+num2;
 system.out.println("The result is"+res);
 system.out.println("Thank you");
