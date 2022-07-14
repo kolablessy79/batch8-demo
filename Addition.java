abf3fa9dba2f2d95ca447feebd6db73be6cf830e
blessy <blessy.ble79@gmail.com>
2022-07-14 18:46:03 +0530

    Changed num1 & num2 values in blessy branch

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ class Addition
 public static void (string args [])
 {
 int num1,num2,res;
-num1=2005;
-num2=4505;
+num1=20057;
+num2=45058;
 res=num1+num2;
 system.out.println("The result is"+res);
 system.out.println("Thank you");
